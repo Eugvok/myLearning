944a16b2e5c26af74524e9e52df2f0e9e25e42d3
Eugene Gladkov <eugvok85@.com>
2023-04-12 11:07:57 +0300

    init: start my lerning folder

---
+++ Elements/template/src/js/modules/calculate.js
@@ -0,0 +1,9 @@
+export function sumas(x,y){
+	let c = x + y,
+		z = new Array();
+	for (let i = 0; i < c; i++){
+		z[i] = c + i;
+	}
+	return z;
+}
+
---
+++ Elements/template/src/js/script.js
@@ -0,0 +1,3 @@
+import {sumas} from "./modules/calculate.js";
+
+console.log(sumas(2,3));
